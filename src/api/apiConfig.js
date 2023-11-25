3deaa30acdcfb9bf071da8d21bd593218b55a735
hridoy00005 <hcb2433@gmail.com>
2023-11-26 03:30:01 +0600

    json token issue fixed

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -4,7 +4,7 @@ import axios from "axios";
 axios.defaults.baseURL = "http://localhost:8000/api";
 // axios.defaults.baseURL = "http://127.0.0.1:8000/api";
 // const token = JSON.parse(localStorage.getItem("persist:auth"))?.token;
-const token = JSON.parse(JSON.parse(localStorage?.getItem("persist:auth") || '""')?.token || '');
+const token = JSON.parse(localStorage?.getItem("persist:auth"))?.token || '';
 // const token = JSON.parse(JSON.parse(localStorage?.getItem("persist:auth") || '')?.token || '');
 
 
